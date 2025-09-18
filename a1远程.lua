a71ccb459f36df26dc2c7387d5cafaa1683f05ac
xxxxxxg <GTAFAW19516968793@qq.com>
2025-09-18 19:32:35 +0800

    Add files via upload

---
+++ a1远程.lua
@@ -0,0 +1,290 @@
+{
+  "6839171747,6516141723": [
+    {
+      "title": "BobDoors",
+      "Tip": "DOORS 脚本",
+      "url": "https://raw.githubusercontent.com/notzanocoddz4/bobdoors/main/main.lua"
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+
+    "125009265613167,99567941238278": [
+    {
+      "title": "墨水脚本",
+      "Tip": "墨水游戏 脚本",
+      "url": "https://rawscripts.net/raw/Ink-Game-BEST-Keyless-Script-for-Ink-Game-53094"
+    },
+    {
+      "title": "博仁",
+      "Tip": "",
+      "url": "https://raw.githubusercontent.com/eikikrkr-ux/bypasok/refs/heads/main/ok"
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+
+    "109983668079237": [
+    {
+      "title": "Best OP New Miranda Tween",
+      "Tip": "偷走脑红 脚本",
+      "content": "Crazy OP New Miranda Tween + Steal tools",
+      "url": "https://pastefy.app/XuS584IO/raw"
+    },
+    {
+      "title": "复制卡密 ↑",
+      "content": "卡密：Key-wk8s-kedo-88wj",
+      "url": "local km='Key-wk8s-kedo-88wj';setclipboard(km)"
+    },
+    {
+      "title": "全新米兰达",
+      "content": "要自己获取卡密，每次解卡只有24小时使用权限",
+      "url": "https://pastefy.app/gU1VDDWt/raw"
+    },
+    {
+      "title": "辣椒脚本",
+      "content": "",
+      "url": "https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+
+    "126509999114328,79546208627805": [
+    {
+      "title": "虚空",
+      "Tip": "在森林中的99夜 脚本",
+      "url": "https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua"
+    },
+    {
+      "title": "冥王星",
+      "url": "https://raw.githubusercontent.com/plutoguy/plutonium/refs/heads/main/script.lua"
+    },
+    {
+      "title": "AXS-HUB",
+      "url": "https://raw.githubusercontent.com/AXS-Main/AXS-Script/refs/heads/main/AXS-HUB/Main/Loader"
+    },
+    {
+      "title": "复制冥王星卡密",
+      "content": "卡密：stealapluto378",
+      "url": "local km='stealapluto378';setclipboard(km)"
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+
+    "137925884276740": [
+    {
+      "title": "AXS-HUB",
+      "Tip": "建造一架飞机 脚本",
+      "url": "https://raw.githubusercontent.com/AXS-HUB2/AXS-ScriptS/refs/heads/main/AXS-HUB/Main/Loader"
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+
+    "90268214441888": [
+    {
+      "title": "建造坦克刷钱脚本",
+      "Tip": "建造坦克 脚本",
+      "url": "https://github.com/GTAFAW/-game/raw/main/建造坦克.lua"
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "111386538027749,133719960214587": [
+    {
+      "title": "超新星",
+      "Tip": "偷饼干 脚本",
+      "url": "https://raw.githubusercontent.com/boplsme/Scripts-/refs/heads/main/Steal-Cookie%5E_%5E.txt"
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "86076978383613,103754275310547": [
+    {
+      "title": "AXS-HUB",
+      "Tip": "猎杀僵尸 脚本",
+      "url": "https://raw.githubusercontent.com/AXS-Main/AXS-Script/refs/heads/main/AXS-HUB/Main/Loader"
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "93978595733734": [
+    {
+      "title": "HM HUB",
+      "Tip": "暴力区 脚本",
+      "url": "https://raw.githubusercontent.com/hamozdz/HM-HUB-Violence-District-BETA/refs/heads/main/Protected_7912138782446260.lua"
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "118915549367482": [
+    {
+      "title": "巧克力",
+      "Tip": "不要吵醒脑袋 脚本",
+      "url": "https://pastefy.app/lQ9YE6yW/raw"
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+     "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+     "": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+    ],
+  
+    "16763882197": [
+    {
+      "title": " ",
+      "Tip": " 脚本",
+      "url": " "
+    },
+    {
+      "title": " ",
+      "content": " ",
+      "url": " "
+    },
+    { "import": "translator" },
+    { "import": "qq" }
+  ]
+}
